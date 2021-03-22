4d0544f7b597514eb6b2b3734b10fa4deda28b8c
mai ha an <maihaan24@gmail.com>
2021-03-22 08:55:06 +0700

    22/3
    22/3

---
+++ DaBang/DaBangHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaBang
+{
+    public class DaBangHelper
+    {
+        char[] bangChuCai = new char[] { 'e', 'w', 'q', 'r', 't', 'u', 'i', 'o', 'p', 
+            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+        char[,] bangTra;
+        public String khoa { get; set; }
+
+        public DaBangHelper()
+        {
+            bangTra = new char[bangChuCai.Length, bangChuCai.Length];
+            for (int i = 0; i < bangChuCai.Length; i++)
+                for (int j = 0; j < bangChuCai.Length; j++)
+                    bangTra[i, j] = bangChuCai[(i + j) % bangChuCai.Length];
+        }
+
+        public String MaHoa(String value)
+        {
+            // Sinh khóa
+            Random rd = new Random();
+            for (int i = 0; i < value.Length; i++)
+                khoa += bangChuCai[rd.Next(0, bangChuCai.Length - 1)];
+
+            // Mã hóa
+            String result = "";
+            for(int i = 0; i < value.Length; i++)
+            {
+                int viTri = -1;
+                for(int j = 0; j < bangChuCai.Length; j++)
+                {
+                    if(bangChuCai[j].ToString().Equals(value[i].ToString()))
+                    {
+                        viTri = j;
+                        break;
+                    }    
+                }
+
+                if (viTri == -1)
+                    result += value[i].ToString();
+                else
+                {
+                    int viTriKhoa = 0;
+                    for(int j = 0; j < bangChuCai.Length; j++)
+                    {
+                        if(bangChuCai[j].ToString().Equals(khoa[i].ToString()))
+                        {
+                            viTriKhoa = j;
+                            break;
+                        }    
+                    }
+                    result += bangTra[viTri, viTriKhoa];
+                }    
+            }
+            return result;
+        }
+
+        public String GiaiMa(String value, String key)
+        {
+            String result = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                int viTri = -1;
+                for (int j = 0; j < bangChuCai.Length; j++)
+                {
+                    if (bangChuCai[j].ToString().Equals(value[i].ToString()))
+                    {
+                        viTri = j;
+                        break;
+                    }
+                }
+
+                if (viTri == -1)
+                    result += value[i].ToString();
+                else
+                {
+                    // Xac dinh ky tu khoa tai vị trí thứ i
+                    char ktKhoa = key[i];
+
+                    // Xác định vị trí của ký tự khóa trong bảng chữ cái
+                    int viTriKhoa = 0;
+                    for(int j = 0; j < bangChuCai.Length; j++)
+                    {
+                        if(bangChuCai[j].Equals(ktKhoa))
+                        {
+                            viTriKhoa = j;
+                            break;
+                        }    
+                    }
+
+                    // Tìm trong hàng hoặc cột thứ (vị trí khóa) ở bảng tra xem giá trị cần giải mã ở vị trí nào
+                    // --> thì giá trị giải mã được là ký tự ở vị trí đố trong bảng chữ cái
+                    int viTriGiaiMa = 0;
+                    for(int j = 0; j < bangChuCai.Length; j++)
+                    {
+                        if(bangTra[viTriKhoa, j].Equals(value[i]))
+                        {
+                            viTriGiaiMa = j;
+                            break;
+                        }    
+                    }
+                    result += bangChuCai[viTriGiaiMa];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ DaBang/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace DaBang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDuLieuDaGiaiMa = new System.Windows.Forms.TextBox();
+            this.btGiaiMa = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDuLieuDaMaHoa = new System.Windows.Forms.TextBox();
+            this.btMaHoa = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDuLieuCanMaHoa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Dữ liệu giải mã được";
+            // 
+            // tbDuLieuDaGiaiMa
+            // 
+            this.tbDuLieuDaGiaiMa.Location = new System.Drawing.Point(12, 330);
+            this.tbDuLieuDaGiaiMa.Multiline = true;
+            this.tbDuLieuDaGiaiMa.Name = "tbDuLieuDaGiaiMa";
+            this.tbDuLieuDaGiaiMa.Size = new System.Drawing.Size(419, 90);
+            this.tbDuLieuDaGiaiMa.TabIndex = 22;
+            // 
+            // btGiaiMa
+            // 
+            this.btGiaiMa.Location = new System.Drawing.Point(12, 278);
+            this.btGiaiMa.Name = "btGiaiMa";
+            this.btGiaiMa.Size = new System.Drawing.Size(75, 23);
+            this.btGiaiMa.TabIndex = 21;
+            this.btGiaiMa.Text = "Giải mã";
+            this.btGiaiMa.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Dữ liệu đã mã hóa";
+            // 
+            // tbDuLieuDaMaHoa
+            // 
+            this.tbDuLieuDaMaHoa.Location = new System.Drawing.Point(12, 181);
+            this.tbDuLieuDaMaHoa.Multiline = true;
+            this.tbDuLieuDaMaHoa.Name = "tbDuLieuDaMaHoa";
+            this.tbDuLieuDaMaHoa.Size = new System.Drawing.Size(419, 90);
+            this.tbDuLieuDaMaHoa.TabIndex = 19;
+            // 
+            // btMaHoa
+            // 
+            this.btMaHoa.Location = new System.Drawing.Point(12, 129);
+            this.btMaHoa.Name = "btMaHoa";
+            this.btMaHoa.Size = new System.Drawing.Size(75, 23);
+            this.btMaHoa.TabIndex = 18;
+            this.btMaHoa.Text = "Mã hóa";
+            this.btMaHoa.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Dữ liệu cần mã hóa";
+            // 
+            // tbDuLieuCanMaHoa
+            // 
+            this.tbDuLieuCanMaHoa.Location = new System.Drawing.Point(12, 32);
+            this.tbDuLieuCanMaHoa.Multiline = true;
+            this.tbDuLieuCanMaHoa.Name = "tbDuLieuCanMaHoa";
+            this.tbDuLieuCanMaHoa.Size = new System.Drawing.Size(419, 90);
+            this.tbDuLieuCanMaHoa.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 434);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbDuLieuDaGiaiMa);
+            this.Controls.Add(this.btGiaiMa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbDuLieuDaMaHoa);
+            this.Controls.Add(this.btMaHoa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDuLieuCanMaHoa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDuLieuDaGiaiMa;
+        private System.Windows.Forms.Button btGiaiMa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDuLieuDaMaHoa;
+        private System.Windows.Forms.Button btMaHoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDuLieuCanMaHoa;
+    }
+}
+
